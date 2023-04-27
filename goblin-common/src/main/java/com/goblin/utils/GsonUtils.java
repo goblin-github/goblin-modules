0720d0485e740757b1f23dea3e7a4b49107c049f
wangpeng <wangpeng@shinemo.com>
2023-04-27 17:29:47 +0800

    feat: 完善gson工具类

--- goblin-common/src/main/java/com/goblin/utils/GsonUtils.java
+++ goblin-common/src/main/java/com/goblin/utils/GsonUtils.java
@@ -1,9 +1,10 @@
 package com.goblin.utils;
 
-import com.google.gson.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.LongSerializationPolicy;
+import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.List;
@@ -12,76 +13,71 @@ import java.util.Map;
 /**
  * Gson序列化工具类
  *
- * @author wangpeng
- * @version v1.0
- * @since 2022/11/17
+ * @author wang
+ * @version v1.1
+ * @since 2023/4/25
  */
 public class GsonUtils {
 
-    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+    private static final Gson gson;
 
-    private static final Gson GSON = new GsonBuilder()
-            .disableHtmlEscaping()
-            .setDateFormat(DATE_FORMAT)
-            .serializeNulls()
-            .create();
-
-    /**
-     * 获取Gson对象
-     */
-    private static Gson getGson() {
-        return GSON;
+    static {
+        gson = new GsonBuilder()
+                .disableHtmlEscaping()
+                .setDateFormat(DATE_FORMAT)
+                .setLongSerializationPolicy(LongSerializationPolicy.DEFAULT)
+                .create();
     }
 
     /**
-     * 将对象转成 json 字符串
+     * 将对象转成json字符串
      *
      * @param obj 要转换的对象
      * @return json 字符串
      */
     public static String toJson(Object obj) {
-        return getGson().toJson(obj);
+        return gson.toJson(obj);
     }
 
     /**
      * 将对象转为json字符串，支持泛型对象
      */
     public static <T> String toJson(T object, Type type) {
-        return getGson().toJson(object, type);
+        return gson.toJson(object, type);
     }
 
     /**
-     * 将 json 字符串转成指定类型的对象
+     * 将json字符串转成指定类型的对象
      *
      * @param json json 字符串
      * @param type 对象类型
      * @param <T>  对象类型
      * @return 对象
      */
-    public static <T> T fromJson(String json, Class<T> type) {
-        return getGson().fromJson(json, type);
+    public static <T> T fromJson(String json, Class<T> type) throws JsonSyntaxException {
+        return gson.fromJson(json, type);
     }
 
     /**
      * 将json字符串转为泛型对象
      */
     public static <T> T fromJson(String json, Type type) throws JsonSyntaxException {
-        return getGson().fromJson(json, type);
+        return gson.fromJson(json, type);
     }
-
 
     /**
      * 将json字符串转为对象列表
      */
     public static <T> List<T> fromJsonList(String json, TypeToken<List<T>> typeToken) throws JsonSyntaxException {
-        return getGson().fromJson(json, typeToken.getType());
+        return gson.fromJson(json, typeToken.getType());
     }
 
     /**
      * 将json字符串转为Map对象
      */
     public static <K, V> Map<K, V> fromJsonMap(String json, TypeToken<Map<K, V>> typeToken) throws JsonSyntaxException {
-        return getGson().fromJson(json, typeToken.getType());
+        return gson.fromJson(json, typeToken.getType());
     }
 
     /**
@@ -90,7 +86,6 @@ public class GsonUtils {
     public static <K, V> Map<K, V> toMap(Object object) {
         Type type = new TypeToken<Map<K, V>>() {
         }.getType();
-        return getGson().fromJson(toJson(object), type);
+        return gson.fromJson(toJson(object), type);
     }
-
 }
